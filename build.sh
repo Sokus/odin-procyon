e2cbd43dbf5cfddd5172118dcdce8137c13abe31
Sokus <soqs687@gmail.com>
2023-02-06 20:17:28 +0100

    Adjust `build.sh` to produce correct extension on Windows

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/bash
 
-odin build src -collection:procyon=src -out=procyon.out -debug
+# shell script might be ran on windows through
+# msys or cygwin, but exe extension is needed
+# to use tools like remedybg
+case "$OSTYPE" in
+  msys*)    extension=exe ;;
+  cygwin*)  extension=exe ;;
+  *)        extension=out;;
+esac
+
+odin build src -collection:procyon=src "-out=procyon.$extension" -debug
